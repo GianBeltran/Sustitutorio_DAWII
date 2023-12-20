b96a17a0e940db95e99554c061f5c0af7324254b
GianBeltran <Gbeltranl0506@gmail.com>
2023-12-19 19:38:27 -0500

    Creacion de proyecto

---
+++ src/main/java/pe/cibertec/edu/examenSusti/ExamenSustiApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.edu.examenSusti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSustiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenSustiApplication.class, args);
+	}
+
+}
